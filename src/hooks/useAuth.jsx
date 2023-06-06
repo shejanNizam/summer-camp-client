9412377c3f085a2d1601e95696d09b7f4b325914
Nizam Shejan <nizam.shejan11@gmail.com>
2023-06-07 01:16:12 +0600

    stracture + TailwindCSS setup

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const useAuth = () => {
+  return (
+    <div>
+      <h3>useAuth</h3>
+    </div>
+  );
+};
+
+export default useAuth;
---
+++ src/router/AdminRoute/AdminRoute.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminRoute = () => {
+  return (
+    <div>
+      <h3>AdminRoute</h3>
+    </div>
+  );
+};
+
+export default AdminRoute;
