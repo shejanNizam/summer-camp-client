11ed240666087edadd14b04c9e80cd10e3b83b1c
Nizam Shejan <nizam.shejan11@gmail.com>
2023-06-07 12:54:46 +0600

    secret key hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCi_hrhbN08fE-NhQ2VLaQtmbqZVqVJhyA",
-  authDomain: "summer-camp-f6243.firebaseapp.com",
-  projectId: "summer-camp-f6243",
-  storageBucket: "summer-camp-f6243.appspot.com",
-  messagingSenderId: "301476824621",
-  appId: "1:301476824621:web:4b7fce3ba69e9cafe71aa4",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
